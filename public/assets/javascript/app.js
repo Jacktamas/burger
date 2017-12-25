05b11faa1a24e2c90ddea7a1e55170345409bf62
Jack Tamas <lovely_gad@yahoo.com>
2017-12-25 10:11:11 -0800

    edited readme

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,15 +1,5 @@
 $(document).ready(function(){
 
-  $.fn.extend({
-    animateCss: function (animationName) {
-      var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-      this.addClass('animated ' + animationName).one(animationEnd, function() {
-        $(this).removeClass('animated ' + animationName);
-      });
-      return this;
-    }
-  });
-
   function createBurger() {
     var newBurger = {
       burger: $(".burger-name").val().trim()
